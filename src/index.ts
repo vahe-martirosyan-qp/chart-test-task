4c17723fbc0499ada67d2c26d794ee7e00c39419
VaheMartirosyan <vahemartirosyan017@icloud.com>
2026-02-02 22:08:14 +0400

    Add shadcn/ui configuration and base setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
